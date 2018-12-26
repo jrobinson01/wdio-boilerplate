e30b77b6150e0927bb7a532c22e1fd0d229c4f37
John Robinson <john.robinson@banno.com>
2018-12-26 18:53:40 -0500

    simplified test to not use shadowDomElement

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,12 +1,27 @@
-describe('FF test', () => {
+describe('setValue test', () => {
   beforeAll(() => {
     browser.url('http://www.youtube.com');
+    browser.waitForExist('#search-input input');
   });
-  describe('setValue test', () => {
+
+  describe('setValue test without command', () => {
     it('should set the value of the input', () => {
-      const input = browser.waitForShadowDomElement(['#search-input input']);
-      input.setValue('webcomponents');
-      expect(input.getValue()).toEqual('webcomponents');
+      const input = browser.element('#search-input input');
+      console.log('direct element:', input);
+      input.setValue('bar');
+      expect(input.getValue()).toEqual('bar');
+    });
+  });
+
+  describe('setValue test with execute', () => {
+    it('should set the value of the input', () => {
+      const input = browser.execute(function() {
+        return document.body.querySelector('#search-input input');
+      }).value;
+      console.log('executed querySelector element:', input);
+      // fails in FF, passes in Chrome
+      input.setValue('baz');
+      expect(input.getValue()).toEqual('baz');
     });
   });
 });
