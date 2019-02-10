eb28a0d9c87b731accb57298b80bd2c5100f5055
John Robinson <john.robinson@banno.com>
2019-02-10 09:33:56 -0500

    remove old test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -12,12 +12,6 @@ describe('setValue test', () => {
         const header = blogApp.shadowRoot().$('header');
         const link = header.$('a-link');
         expect(link.getAttribute('href')).toBe('/');
-        // const login = $('bannoweb-login');
-        //  const loginForm = login.shadowRoot().$('bannoweb-login-form');
-        // const input = loginForm.shadowRoot().$('input[type=text]');
-        // input.waitForEnabled();
-        // input.setValue('hello');
-        // expect(input.getValue()).toBe('hello');
       } catch(e) {
         console.error('error:', e);
       }
